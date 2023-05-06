abccdeedf1180279b2cf5f9da4e122d66344d0d4
Edward Joseph Pegollo <>
2023-05-06 20:08:25 +0800

    Setup MongoDB

---
+++ app/src/main/java/com/edwardjdp/pointofuapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.edwardjdp.pointofuapp.util
+
+object Constants {
+    const val APP_ID = "pointofuapp-qounu"
+    const val CLIENT_ID = "969705978595-qvdq92oure3ps5ali21kc08vontfvegu.apps.googleusercontent.com"
+}
