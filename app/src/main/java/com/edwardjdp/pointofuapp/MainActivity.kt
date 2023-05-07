9604cb81dc0f47e550ee96e8efb8aea35a5bdb5f
Edward Joseph Pegollo <>
2023-05-07 12:28:55 +0800

    Add Authentication screen

--- app/src/main/java/com/edwardjdp/pointofuapp/MainActivity.kt
+++ app/src/main/java/com/edwardjdp/pointofuapp/MainActivity.kt
@@ -4,24 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.edwardjdp.pointofuapp.navigation.Screen
 import com.edwardjdp.pointofuapp.navigation.SetupNavGraph
 import com.edwardjdp.pointofuapp.ui.theme.PointOfUAppTheme
+import com.edwardjdp.pointofuapp.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             PointOfUAppTheme {
                 val navController = rememberNavController()
                 SetupNavGraph(
-                    startDestination = Screen.Authentication.route,
+                    startDestination = getStartDestination(),
                     navController = navController
                 )
 
             }
         }
+    }
+}
+
+private fun getStartDestination(): String {
+    val user = App.create(APP_ID).currentUser
+    return if (user != null && user.loggedIn) {
+        Screen.Home.route
+    } else {
+        Screen.Authentication.route
     }
 }
